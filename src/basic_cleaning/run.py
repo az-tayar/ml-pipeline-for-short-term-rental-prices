4827ecac7b3ab44bbd58dca6703b652495c0d22d
az-tayar <az.tayar23@gmail.com>
2026-07-04 22:46:21 -0400

    adding preprocessing step for filtering data outside the geographical boundaries

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -30,6 +30,12 @@ def go(args):
     # converting the 'last_review' col to datetime object instead 
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy() 
+
+    logger.info("Cleaned data has %s rows and %s columns", *df.shape)
+
+    # Save cleaned data
     df.to_csv(args.output_artifact, index=False)
 
     artifact = wandb.Artifact(
